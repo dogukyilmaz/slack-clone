c28aea85b71ccf374549c92007c4087f9e7e743a
Kaan Yilmaz <douscriptist@gmail.com>
2020-08-21 00:05:32 +0300

    add fundamental components to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Landing;
