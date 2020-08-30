f60727937b7292d443ddb59d8d18d6989ed9cc93
Kaan Yilmaz <douscriptist@gmail.com>
2020-08-30 22:20:45 +0300

    upload image & show in chat

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Segment, Input, Button } from "semantic-ui-react";
+import { v4 } from "uuid";
 import firebase from "../../firebase";
 import FileModal from "./FileModal";
 
@@ -8,6 +9,10 @@ const MessageForm = ({ messagesRef, channel, user }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [modal, setModal] = useState(false);
+  const [storageRef] = useState(firebase.storage().ref());
+  const [uploadState, setUploadState] = useState("");
+  // const [uploadTask, setUploadTask] = useState(null);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
   const sendMessage = async () => {
     if (message && message.trim().length > 0) {
@@ -27,21 +32,87 @@ const MessageForm = ({ messagesRef, channel, user }) => {
     }
   };
 
-  const messageBuilder = () => {
-    return {
+  const messageBuilder = (fileUrl = null) => {
+    const returnMsg = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       user: {
         id: user.uid,
         name: user.displayName,
         avatar: user.photoURL,
       },
-      content: message,
     };
+    if (fileUrl) {
+      returnMsg["image"] = fileUrl;
+    } else {
+      returnMsg["content"] = message;
+    }
+    return returnMsg;
   };
 
-  const uploadFile = (file, metadata) => {
-    console.log(file);
-    console.log(metadata);
+  const sendFileMessage = (fileUrl, ref, uploadPath) => {
+    ref
+      .child(uploadPath)
+      .push()
+      .set(messageBuilder(fileUrl))
+      .then(() => {
+        setUploadState("done");
+      })
+      .catch((err) => {
+        setError(err);
+        console.log(err, "1");
+      });
+  };
+
+  const uploadFile = async (file, metadata) => {
+    // pathToUpload = channel.id
+    // ref = messagesRef
+    var filePath = `chat/public/${v4()}.jpg`;
+
+    setUploadState("uploading");
+
+    const uploadTask = storageRef.child(filePath).put(file, metadata);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        let percentUploaded = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setUploadPercentage(percentUploaded);
+        // switch (snapshot.state) {
+        //   case firebase.storage.TaskState.PAUSED: // or 'paused'
+        //     console.log("Upload is paused");
+        //     break;
+        //   case firebase.storage.TaskState.RUNNING: // or 'running'
+        //     console.log("Upload is running");
+        //     break;
+        //   default:
+        //     return;
+        // }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        setError(error);
+        console.log(error, "2");
+
+        setUploadState("error");
+      },
+      () => {
+        // Handle successful uploads on complete
+        uploadTask.snapshot.ref
+          .getDownloadURL()
+          .then((downloadURL) => {
+            console.log(downloadURL);
+            console.log(messagesRef);
+            console.log(filePath);
+            sendFileMessage(downloadURL, messagesRef, channel.id);
+          })
+          .catch((err) => {
+            setError(err);
+            console.log(err, "3");
+            setUploadState("error");
+          });
+      }
+    );
   };
 
   const addEmoji = () => {};
